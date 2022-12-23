7f38f999fac0fc5e1fb6c1943d2dcb53d4cae1b9
Miguel Antonio Bastidas Verdin <75141067+ELBASTIDAAS@users.noreply.github.com>
2022-12-23 10:00:06 -0800

    login vue

---
+++ sistemaapi/src/main.js
@@ -0,0 +1,24 @@
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+
+Vue.config.productionTip = false
+// Make BootstrapVue available throughout your project
+Vue.use(BootstrapVue)
+// Optionally install the BootstrapVue icon components plugin
+Vue.use(IconsPlugin)
+// Make axios available throughout your project
+Vue.use(VueAxios, axios)
+
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+
+
+new Vue({
+  router,
+  render: h => h(App)
+}).$mount('#app')
